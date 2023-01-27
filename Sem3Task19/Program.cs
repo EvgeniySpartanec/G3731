cae212a5deb318917a7338d2d5de466d88ea285d
Evgeniy <ev.batishchev@mail.ru>
2023-01-27 12:00:37 +0300

    add solution for task 17,18,19,20,21,22,23

---
+++ Sem3Task19/Program.cs
@@ -0,0 +1,32 @@
+﻿//Напишите программу, которая принимает 
+//на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
+
+Console.Write("Укажите число: ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number){
+  if (number[0]==number[4] || number[1]==number[3]){
+    Console.WriteLine($"Данное число: {number} - палиндром.");
+  }
+  else Console.WriteLine($"Данное число: {number} - НЕ палиндром.");
+}
+
+if (number!.Length == 5){
+  CheckingNumber(number);
+}
+else Console.WriteLine($"Укажите корректное число");
+
+
+
+
+
+
+
+
+
+
+
+
