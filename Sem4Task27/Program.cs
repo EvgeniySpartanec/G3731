9e3a62b4e45e55f25e44b0c184dd1d371e68e809
Evgeniy <ev.batishchev@mail.ru>
2023-01-29 00:29:54 +0300

    add solution for task 24,25,26,27,28,29,30

---
+++ Sem4Task27/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+//Задаем данные для ввода пользователя
+Console.WriteLine();
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+  int SumNumber(int numberN){
+    //Указываем переменные
+    int counter = Convert.ToString(numberN).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++){
+      advance = numberN - numberN % 10;
+      result = result + (numberN - advance);
+      numberN = numberN / 10;
+    }
+   return result;
+  }
+
+int sumNumber = SumNumber(numberN);
+//выводим данные для пользователя
+Console.WriteLine("Сумма цифр в числе: " + sumNumber);
+
+
+
+
+
+
+
+
